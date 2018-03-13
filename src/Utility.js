0eca0a7dfdf71cfaf64e13530bc5f273b1d06c26
Bria Gay-Mitchell <bria.gaymitchell@gmail.com>
2018-03-12 21:27:23 -0400

    add ageInYears utility function

--- src/Utility.js
+++ src/Utility.js
@@ -1,3 +1,11 @@
 function fullName(firstName, lastName) {
   return `${firstName} ${lastName}`;
 }
+
+function ageInYears(birthdayString) {
+  const birthday = new Date(birthdayString);
+  const ageInMilliseconds = Date.now() - birthday;
+  const millisecondsPerYear = 31557600000;
+
+  return Math.floor(ageInMilliseconds / millisecondsPerYear);
+}
